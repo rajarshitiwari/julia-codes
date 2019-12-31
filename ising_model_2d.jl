dbd3b82c024074bb6b84eb9cebd28e856c1e51ca
rajarshitiwari <rajarshi84@gmail.com>
2019-12-31 00:21:26 +0000

    major change, significantly faster. made most part into functions, removed global vars.

--- ising_model_2d.jl
+++ ising_model_2d.jl
@@ -91,7 +91,7 @@ function calculate_average_sssq(spins::Array{Int64, 2})
     return [total_s, total_ssq]
 end # end subroutine calculate_average_sssq
 
-function create_indices(lsize::Int64)
+function create_indices(lsize::Int64)::Array{Int64, 2}
     # INTEGER, INTENT(IN) :: LSIZE
 
     indices = zeros(Int64, (lsize, lsize))
@@ -107,7 +107,7 @@ generate_random_spin() = rand() >= 0.5 ? 1 : -1
 
 ### %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ###
 ### TAKE THE SPIN CONFIGURATION AND CALCULATE SI.SJ      ###
-function calculate_si_sj(spins::Array{Int64, 2})::Array{Float64, 2}
+function calculate_si_sj(spins::Array{Int64, 2}, indices::Array{Int64, 2})::Array{Float64, 2}
     # INTEGER, DIMENSION(:,:), INTENT(IN) :: SPINS
     # INTEGER, DIMENSION(:,:), INTENT(OUT) :: SI_SJ
 
@@ -130,12 +130,13 @@ function calculate_si_sj(spins::Array{Int64, 2})::Array{Float64, 2}
     return si_sj
 end # SUBROUTINE CALCULATE_SI_SJ
 
-function selected_sq_from_si_sj(si_sj::Array{Float64}, que::Vector{Int64}, lsize::Int64)::Float64
+function selected_sq_from_si_sj(si_sj::Array{Float64}, que::Vector{Int64}, indices::Array{Int64, 2})::Float64
     # INTEGER, DIMENSION(:,:), INTENT(IN) :: SI_SJ
     # INTEGER, DIMENSION(:), INTENT(IN) :: QUE
     # INTEGER, INTENT(IN) :: LSIZE
     # REAL(8), INTENT(OUT) :: STRUCTURE_FACTOR
 
+    lsize = size(indices, 1)
     nsize::Int64 = lsize * lsize
     coeff::Float64 = 2.0 * PI/ lsize
     normalize = nsize^2
@@ -171,10 +172,8 @@ end # SUBROUTINE SELECTED_SQ_FROM_SI_SJ
 ### %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ###
 ### GIVEN SI.SJ FOR THE SPIN CONFIGURATION, CALCULATE    ###
 ### THE STRUCTURE FACTOR S(Q) FROM THIS                  ###
-function structure_factor_from_si_sj(si_sj::Array{Float64}, lsize::Int64)::Array{Float64, 2}
-    # INTEGER, DIMENSION(:,:), INTENT(IN) :: SI_SJ
-    # REAL(8), DIMENSION(:,:), INTENT(OUT) :: STRUCTURE_FACTOR
-
+function structure_factor_from_si_sj(si_sj::Array{Float64}, indices::Array{Int64, 2})::Array{Float64, 2}
+    lsize = size(indices, 1)
     structure_factor = zeros(lsize, lsize)
     que = zeros(Int64, 2)
     
@@ -185,7 +184,7 @@ function structure_factor_from_si_sj(si_sj::Array{Float64}, lsize::Int64)::Array
         for q2 = 1:lsize
             que[2] = q2 - 1
             #
-            structure_factor[q1, q2] = selected_sq_from_si_sj(si_sj, que, lsize)
+            structure_factor[q1, q2] = selected_sq_from_si_sj(si_sj, que, indices::Array{Int64, 2})
             #
         end # DO LOOP_Q2
     end # DO LOOP_Q1
@@ -202,225 +201,231 @@ end # SUBROUTINE STRUCTURE_FACTOR_FROM_SI_SJ
 
 const lsize = 20
 const nsize = lsize * lsize
-#   UNIT_ES   = 100
-#   UNIT_MX   = 101
-#   UNIT_SP   = 102
-#   UNIT_SQ   = 103
-#   UNIT_SISJ = 104
-#   UNIT_EPS  = 105
-#   UNIT_ACC  = 106
-#   UNIT_TMP  = 111
 
-#   DEFINE THE TIME OF SWEEP AND NUMBER OF STEPS TO EQUILIBRIATE.
-global maxmcsweep = 1000 #0
-global nequil = 500 #0
-global ndel = 1
-global number_of_temperature = 60
+
+function main(jey1::Float64, jey2::Float64, magnetic_field::Float64, maxmcsweep::Int64, nequil::Int64, ndel::Int64, Temperatures::Vector{Float64})
+    
+    #   UNIT_ES   = 100
+    #   UNIT_MX   = 101
+    #   UNIT_SP   = 102
+    #   UNIT_SQ   = 103
+    #   UNIT_SISJ = 104
+    #   UNIT_EPS  = 105
+    #   UNIT_ACC  = 106
+    #   UNIT_TMP  = 111
+    # number_of_temperature = 60
+    # max_temperature = 3.0 * abs(jey1)
+    # delta_t = max_temperature / (number_of_temperature - 1)
+    
+    si_sj = zeros(nsize, nsize)
+    global sf_ising = zeros(lsize, lsize) #   ALLOCATE(SF_ISING(0:LSIZE-1,0:LSIZE-1))
+    
+    global spins_lattice = zeros(Int64, (lsize, lsize))
+
+    global indices = create_indices(lsize)
+    global minusplus = [circshift(Vector(1:lsize), 1) circshift(Vector(1:lsize), -1)]
+    
+    #   OPEN(UNIT_TMP, FILE = 'ising_2d.inp')
+    #   WRITE(UNIT_TMP,'("JEY1            = ",F15.5)')JEY1
+    #   WRITE(UNIT_TMP,'("JEY2            = ",F15.5)')JEY2
+    #   WRITE(UNIT_TMP,'("LSIZE           = ",I15)')LSIZE
+    #   WRITE(UNIT_TMP,'("NSIZE           = ",I15)')NSIZE
+    #   WRITE(UNIT_TMP,'("SWEEP LENGTH    = ",I15)')MAXMCSWEEP
+    #   WRITE(UNIT_TMP,'("EQUIL LENGTH    = ",I15)')NEQUIL
+    #   WRITE(UNIT_TMP,'("NO. OF T        = ",I15)')NUMBER_OF_TEMPERATURE
+    #   WRITE(UNIT_TMP,'("MAXIMUM T       = ",F15.5)')MAX_TEMPERATURE
+    #   WRITE(UNIT_TMP,'("DELTA T         = ",F15.5)')DELTA_T
+    #   WRITE(UNIT_TMP,'("MAGNETIC FIELD  = ",3(F15.5,1X))')MAGNETIC_FIELD
+    
+    #   CLOSE(UNIT_TMP)
+    
+    #   WRITE(*,'("JEY1            = ",F15.5)')JEY1
+    #   WRITE(*,'("JEY2            = ",F15.5)')JEY2
+    #   WRITE(*,'("LSIZE           = ",I15)')LSIZE
+    #   WRITE(*,'("NSIZE           = ",I15)')NSIZE
+    #   WRITE(*,'("SWEEP LENGTH    = ",I15)')MAXMCSWEEP
+    #   WRITE(*,'("EQUIL LENGTH    = ",I15)')NEQUIL
+    #   WRITE(*,'("NO. OF T        = ",I15)')NUMBER_OF_TEMPERATURE
+    #   WRITE(*,'("MAXIMUM T       = ",F15.5)')MAX_TEMPERATURE
+    #   WRITE(*,'("DELTA T         = ",F15.5)')DELTA_T
+    #   WRITE(*,'("MAGNETIC FIELD  = ",3(F15.5,1X))')MAGNETIC_FIELD
+
+    #GENERATE THE SNAPSHOT OF SPINS (RANDOM CONFIGURATION)#
+
+    for i1 = 1:lsize, i2 = 1:lsize
+        #SPINS_LATTICE(I1,I2) = 1
+        spins_lattice[i1, i2] = generate_random_spin()
+    end
+    unit_tmp = open("starting_spins.dat", "w")
+    for i1 = 1:lsize
+        for i2 = 1:lsize
+            print(unit_tmp, string(spins_lattice[i1, i2], " "))
+        end
+        print(unit_tmp,"\n")
+    end
+    close(unit_tmp)
+
+    #   #=====================================#
+    #   #=======OPEN THE FILES TO WRITE=======#
+    #   OPEN(UNIT_ES, FILE = 'energy_susceptibility.dat')
+    #   OPEN(UNIT_MX, FILE = 'magnetization_chai.dat')
+    #   OPEN(UNIT_SP, FILE = 'spin_configurations.dat')
+    #   OPEN(UNIT_SQ, FILE = 'structure_factors.dat')
+    #   OPEN(UNIT_SISJ, FILE = 'avgsisj.dat')
+    #   OPEN(UNIT_ACC, FILE = 'acceptence.dat')
+    
+    #   WRITE(UNIT_ES,'(3(A15,1X))')'TEMPERATURE','AVERAGE_ENERGY','SPECIFIC_HEAT'
+    #   WRITE(UNIT_MX,'(6(A15,1X))')'TEMPERATURE','SQ_FERRO','SQ_G_TYPE_AF',&
+    #        &'CHAI','S','SSQ'
+    #   #======================================#
+    
+    total_energy = total_energy_ising_2d(spins_lattice, jey1, jey2, magnetic_field)
+    println("TOTAL ENERGY IN STARTING = ", total_energy / nsize)
+    
+    # println(spins_lattice)
+    println("STARTING THE T LOOP")
+
+    # total_s = sum(spins_lattice)
+    
+    previous_energy = total_energy
+    
+    # temperature = max_temperature
+    println(Temperatures[1])
+    count_temp = 0
+    
+    for temperature in Temperatures # temperature_loop
+        #
+        global mcsweep = 1
+        count = 0
+        oldcount = 0
+        outcount = 0
+        global avg_sisj = zeros(nsize, nsize)
+        # global avg_sisj .= 0.0
+        global avge = 0.0
+        global avgesq = 0.0
+        magnetization = 0.0
+        sqaf = 0.0
+        global average_s = 0.0
+        global average_ssq = 0.0
+        chai = 0.0
+        global imc = 0
+        global ifail = 0
+        global ipass = 0
+        # CALL CPU_TIME(TIME1) #TIME1 = X05BAF()
+        # MC_LOOP
+        @time while mcsweep <= maxmcsweep
+            #
+            # SWEEP_I1, SWEEP_I2
+            for i1 = 1:lsize, i2 = 1:lsize
+                tmp_spin = - spins_lattice[i1, i2] # FLIP THE TEMP SPIN
+                ########################################
+                # CALCULATE THE ENERGY DIFFERENCE NOW  #
+                ########################################
+                energy_difference = get_energy_diff(spins_lattice, tmp_spin, jey1, jey2, magnetic_field, minusplus, Vector{Int64}([i1, i2]))
+                #
+                flag = metro_polis(energy_difference, temperature)
+                if flag
+                    ipass += 1
+                    spins_lattice[i1, i2] = tmp_spin # update the temp spin
+                    final_energy = previous_energy + energy_difference
+                else
+                    ifail += 1
+                    final_energy = previous_energy
+                end
+                imc += 1
+            end                     # sweep_i1, sweep_i2
+            
+            #
+            if mcsweep > nequil
+                count += 1
+                # taking_avg
+                if count - oldcount == ndel
+                    outcount += 1
+                    
+                    si_sj .= calculate_si_sj(spins_lattice, indices)
+                    total_s, s_square = calculate_average_sssq(spins_lattice)
+                    # total_s = sum(spins_lattice(:,:))
+                    # s_square = total_s**2
+                    #
+                    total_energy = total_energy_ising_2d(spins_lattice, jey1, jey2, magnetic_field)
+                    #
+                    average_s += total_s
+                    average_ssq += s_square
+                    avg_sisj .+= si_sj
+                    avgesq += total_energy^2
+                    avge += total_energy
+                    #
+                    oldcount = count
+                end # taking_avg
+            end
+            mcsweep += 1
+        end                         # end mc_loop while
+        
+        divide = outcount * 1.0
+        avg_sisj .= avg_sisj / divide
+        avge = avge / divide
+        avgesq = avgesq / divide
+        average_s = average_s / divide
+        average_ssq = average_ssq / divide
+        # write(unit_acc,'(3(f15.10,1x))')temperature,dble(ipass)/dble(imc),dble(ifail)/dble(imc)
+        
+        # ###########################################################
+        # ###########################################################
+        # call cpu_time(time2) #time2 = x05baf()#
+        # write(*,"(a,2x,f15.10)",advance='no')'running program at t =',temperature
+        # write(*,'(3x,"time taken =",f12.2," seconds")',advance='no')(time2-time1)
+        
+        # call cpu_time(tsf1)        #tsf1 = x05baf()
+        @time sf_ising = structure_factor_from_si_sj(avg_sisj, indices)
+        # call cpu_time(tsf2)        #tsf2 = x05baf()
+        max_sq = maximum(sf_ising)
+        # write(*,'(3x,"time for s(q) =",1x,f12.2,1x,"seconds. maximum s(q) =",1x,f20.10)')tsf2-tsf1,max_sq
+        println("count = ", count_temp, " Temperature = ", temperature)
+        c_v = (avgesq - (avge)^2) / nsize
+        c_v = c_v / (temperature^2)
+        avge = avge / nsize
+        
+        chai = average_ssq / nsize - average_s^2 / nsize
+        chai = chai / temperature
+        sqaf = sf_ising[Int(lsize/2), Int(lsize/2)]
+        magnetization = sf_ising[1, 1]
+        #
+        # write(unit_es,'(2(f20.10,1x),e20.10)')temperature,avge,c_v
+        # write(unit_mx,'(6(f20.10,1x))')temperature,magnetization,sqaf,chai,average_s/dble(nsize),average_ssq/dble(nsize)
+        # write(unit_sp,*)'t=',temperature
+        for i1 = 1:lsize, i2 = 1:lsize
+            # write(unit_sp,'(3(i2,1x))')i1,i2,spins_lattice(i1,i2)
+            qstr = string(i1 - 1, "_", i2 - 1)
+            # write(unit_sisj,'(3(i2,1x))')i1,i2,avg_sisj(1,indices(i1,i2))
+            # write(unit_sq,'(a9,1x,1(f20.10,1x),e20.10)')qstr,temperature,sf_ising(i1-1,i2-1)
+        end
+        # write(unit_sp,*)
+        # write(unit_sq,*)
+        # temperature += -delta_t
+        count_temp += 1
+    end # do temperature_loop
+
+end                             # end main
+
 
 #   #======READ THE INPUT VARIABLES FIRSTLY FROM ARGUMENT
 #   #IF THAT NOT GIVEN THEN FROM STANDARD INPUT=======#
-global jey1 = -1.0
-global jey2 =  0.0
+jey1 = -1.0
+jey2 =  0.0
+magnetic_field = 0.0
 #   PRINT*,"ENTER JEY2,MAX TEMPERATURE"
 #   READ(*,*)JEY2,MAX_TEMPERATURE #JEY2 = 0.5D0
 
-global magnetic_field = 0.0
-global max_temperature = 3.0 * abs(jey1)
-global delta_t = max_temperature / (number_of_temperature - 1)
-
-si_sj = zeros(nsize, nsize)
-global sf_ising = zeros(lsize, lsize) #   ALLOCATE(SF_ISING(0:LSIZE-1,0:LSIZE-1))
-
-global spins_lattice = zeros(Int64, (lsize, lsize))
-
-
-#   ALLOCATE(MINUSPLUS(LSIZE,2),ONETON(LSIZE))
-
-global indices = create_indices(lsize)
-global minusplus = [circshift(Vector(1:lsize), 1) circshift(Vector(1:lsize), -1)]
-
-#   OPEN(UNIT_TMP, FILE = 'ising_2d.inp')
-#   WRITE(UNIT_TMP,'("JEY1            = ",F15.5)')JEY1
-#   WRITE(UNIT_TMP,'("JEY2            = ",F15.5)')JEY2
-#   WRITE(UNIT_TMP,'("LSIZE           = ",I15)')LSIZE
-#   WRITE(UNIT_TMP,'("NSIZE           = ",I15)')NSIZE
-#   WRITE(UNIT_TMP,'("SWEEP LENGTH    = ",I15)')MAXMCSWEEP
-#   WRITE(UNIT_TMP,'("EQUIL LENGTH    = ",I15)')NEQUIL
-#   WRITE(UNIT_TMP,'("NO. OF T        = ",I15)')NUMBER_OF_TEMPERATURE
-#   WRITE(UNIT_TMP,'("MAXIMUM T       = ",F15.5)')MAX_TEMPERATURE
-#   WRITE(UNIT_TMP,'("DELTA T         = ",F15.5)')DELTA_T
-#   WRITE(UNIT_TMP,'("MAGNETIC FIELD  = ",3(F15.5,1X))')MAGNETIC_FIELD
-
-#   CLOSE(UNIT_TMP)
-
-#   WRITE(*,'("JEY1            = ",F15.5)')JEY1
-#   WRITE(*,'("JEY2            = ",F15.5)')JEY2
-#   WRITE(*,'("LSIZE           = ",I15)')LSIZE
-#   WRITE(*,'("NSIZE           = ",I15)')NSIZE
-#   WRITE(*,'("SWEEP LENGTH    = ",I15)')MAXMCSWEEP
-#   WRITE(*,'("EQUIL LENGTH    = ",I15)')NEQUIL
-#   WRITE(*,'("NO. OF T        = ",I15)')NUMBER_OF_TEMPERATURE
-#   WRITE(*,'("MAXIMUM T       = ",F15.5)')MAX_TEMPERATURE
-#   WRITE(*,'("DELTA T         = ",F15.5)')DELTA_T
-#   WRITE(*,'("MAGNETIC FIELD  = ",3(F15.5,1X))')MAGNETIC_FIELD
-
-#GENERATE THE SNAPSHOT OF SPINS (RANDOM CONFIGURATION)#
-
-for i1 = 1:lsize, i2 = 1:lsize
-    #SPINS_LATTICE(I1,I2) = 1
-    spins_lattice[i1, i2] = generate_random_spin()
-end
-unit_tmp = open("starting_spins.dat", "w")
-for i1 = 1:lsize
-    for i2 = 1:lsize
-        print(unit_tmp, string(spins_lattice[i1, i2], " "))
-    end
-    print(unit_tmp,"\n")
-end
-close(unit_tmp)
-
-#   #=====================================#
-#   #=======OPEN THE FILES TO WRITE=======#
-#   OPEN(UNIT_ES, FILE = 'energy_susceptibility.dat')
-#   OPEN(UNIT_MX, FILE = 'magnetization_chai.dat')
-#   OPEN(UNIT_SP, FILE = 'spin_configurations.dat')
-#   OPEN(UNIT_SQ, FILE = 'structure_factors.dat')
-#   OPEN(UNIT_SISJ, FILE = 'avgsisj.dat')
-#   OPEN(UNIT_ACC, FILE = 'acceptence.dat')
-
-#   WRITE(UNIT_ES,'(3(A15,1X))')'TEMPERATURE','AVERAGE_ENERGY','SPECIFIC_HEAT'
-#   WRITE(UNIT_MX,'(6(A15,1X))')'TEMPERATURE','SQ_FERRO','SQ_G_TYPE_AF',&
-#        &'CHAI','S','SSQ'
-#   #======================================#
-
-total_energy = total_energy_ising_2d(spins_lattice, jey1, jey2, magnetic_field)
-println("TOTAL ENERGY IN STARTING = ", total_energy / nsize)
-
-# println(spins_lattice)
-println("STARTING THE T LOOP")
-
-# total_s = sum(spins_lattice)
-
-previous_energy = total_energy
-
-temperature = max_temperature
-println(temperature)
-count_temp = 0
-
-while temperature::Float64 > 0.0         # temperature_loop
-    #
-    global mcsweep = 1
-    count = 0
-    oldcount = 0
-    outcount = 0
-    global avg_sisj = zeros(nsize, nsize)
-    # global avg_sisj .= 0.0
-    global avge = 0.0
-    global avgesq = 0.0
-    magnetization = 0.0
-    sqaf = 0.0
-    global average_s = 0.0
-    global average_ssq = 0.0
-    chai = 0.0
-    global imc = 0
-    global ifail = 0
-    global ipass = 0
-    # CALL CPU_TIME(TIME1) #TIME1 = X05BAF()
-    # MC_LOOP
-    @time while mcsweep <= maxmcsweep
-        #
-        # SWEEP_I1, SWEEP_I2
-        for i1 = 1:lsize, i2 = 1:lsize
-            tmp_spin = - spins_lattice[i1, i2] # FLIP THE TEMP SPIN
-            ########################################
-            # CALCULATE THE ENERGY DIFFERENCE NOW  #
-            ########################################
-            energy_difference = get_energy_diff(spins_lattice, tmp_spin, jey1, jey2, magnetic_field, minusplus, Vector{Int64}([i1, i2]))
-            #
-            flag = metro_polis(energy_difference, temperature)
-            if flag
-                ipass += 1
-                spins_lattice[i1, i2] = tmp_spin # update the temp spin
-                final_energy = previous_energy + energy_difference
-            else
-                ifail += 1
-                final_energy = previous_energy
-            end
-            imc += 1
-        end                     # sweep_i1, sweep_i2
-        
-        #
-        if mcsweep > nequil
-            count += 1
-            # taking_avg
-            if count - oldcount == ndel
-                outcount += 1
-
-                si_sj .= calculate_si_sj(spins_lattice)
-                total_s, s_square = calculate_average_sssq(spins_lattice)
-                # total_s = sum(spins_lattice(:,:))
-                # s_square = total_s**2
-                #
-                total_energy = total_energy_ising_2d(spins_lattice, jey1, jey2, magnetic_field)
-                #
-                average_s += total_s
-                average_ssq += s_square
-                avg_sisj .+= si_sj
-                avgesq += total_energy^2
-                avge += total_energy
-                #
-                oldcount = count
-            end # taking_avg
-        end
-        mcsweep += 1
-    end                         # end mc_loop while
-
-    divide = outcount * 1.0
-    avg_sisj .= avg_sisj / divide
-    avge = avge / divide
-    avgesq = avgesq / divide
-    average_s = average_s / divide
-    average_ssq = average_ssq / divide
-    # write(unit_acc,'(3(f15.10,1x))')temperature,dble(ipass)/dble(imc),dble(ifail)/dble(imc)
-    
-    # ###########################################################
-    # ###########################################################
-    # call cpu_time(time2) #time2 = x05baf()#
-    # write(*,"(a,2x,f15.10)",advance='no')'running program at t =',temperature
-    # write(*,'(3x,"time taken =",f12.2," seconds")',advance='no')(time2-time1)
-    
-    # call cpu_time(tsf1)        #tsf1 = x05baf()
-    @time sf_ising = structure_factor_from_si_sj(avg_sisj, lsize)
-    # call cpu_time(tsf2)        #tsf2 = x05baf()
-    max_sq = maximum(sf_ising)
-    # write(*,'(3x,"time for s(q) =",1x,f12.2,1x,"seconds. maximum s(q) =",1x,f20.10)')tsf2-tsf1,max_sq
-    println("count = ", count_temp, " Temperature = ", temperature)
-    c_v = (avgesq - (avge)^2) / nsize
-    c_v = c_v / (temperature^2)
-    avge = avge / nsize
-    
-    chai = average_ssq / nsize - average_s^2 / nsize
-    chai = chai / temperature
-    sqaf = sf_ising[Int(lsize/2), Int(lsize/2)]
-    magnetization = sf_ising[1, 1]
-    #
-    # write(unit_es,'(2(f20.10,1x),e20.10)')temperature,avge,c_v
-    # write(unit_mx,'(6(f20.10,1x))')temperature,magnetization,sqaf,chai,average_s/dble(nsize),average_ssq/dble(nsize)
-    # write(unit_sp,*)'t=',temperature
-    for i1 = 1:lsize, i2 = 1:lsize
-        # write(unit_sp,'(3(i2,1x))')i1,i2,spins_lattice(i1,i2)
-        qstr = string(i1 - 1, "_", i2 - 1)
-        # write(unit_sisj,'(3(i2,1x))')i1,i2,avg_sisj(1,indices(i1,i2))
-        # write(unit_sq,'(a9,1x,1(f20.10,1x),e20.10)')qstr,temperature,sf_ising(i1-1,i2-1)
-    end
-    # write(unit_sp,*)
-    # write(unit_sq,*)
-    global temperature += -delta_t
-    global count_temp += 1
-end # do temperature_loop
-
-
-
+#   DEFINE THE TIME OF SWEEP AND NUMBER OF STEPS TO EQUILIBRIATE.
+maxmcsweep = 1000 #0
+nequil = 500 #0
+ndel = 1
+MaxTemperature = 3.0 * abs(jey1)
+MinTemperature = 0.0
+number_of_temperature = 60
+Temperatures = Vector(LinRange(MaxTemperature, MinTemperature, number_of_temperature))
+println(Temperatures)
+main(jey1, jey2, magnetic_field, maxmcsweep, nequil, ndel, Temperatures)
 
 # CLOSE(UNIT_ES)
 # CLOSE(UNIT_MX)
